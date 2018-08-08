512a563c59aa36fca23f00325d8046445b7ff00c
ArturWax <armyan.exclusive51@gmail.com>
2018-08-08 22:50:01 +0500

    Rev 20
    ArturWax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-$('.collapse').on('show.bs.collapse', function() {
-    $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
-});
-$('.collapse').on('hide.bs.collapse', function() {
-    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        }else{
+            $('#top').fadeOut();
+        }
+    })
+    $('#top').on('click', function () {
+        $('body,html').animate({
+            scrollTop: 0
+        },2000)
+    });
 });
