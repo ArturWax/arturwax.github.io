4f5416674862d6ff601e645f71d2894e6c7f9d2a
ArturWax <armyan.exclusive51@gmail.com>
2018-08-19 13:08:08 +0500

    Rev26
    ArturWax

--- projects/oxxo — копия/js/script.js
+++
@@ -1,6 +0,0 @@
-$('.collapse').on('show.bs.collapse', function() {
-    $(this).prev().find('.fa').removeClass().addClass('fa collapse-minus');
-});
-$('.collapse').on('hide.bs.collapse', function() {
-    $(this).prev().find('.fa').removeClass().addClass('fa collapse-plus');
-});
